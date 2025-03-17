66cd11ff79ddc41a7953e345b6a0d677bb4bd859
Oliver2828 <oliverezenwaka95@gmail.com>
2025-03-17 19:03:03 +0000

    About

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import About1 from '../components/About-Components/About1'
+import About2 from '../components/About-Components/About2'
+import About3 from '../components/About-Components/About3'
 
 function About() {
   return (
     <>
         <About1/>
+        <About2/>
+        <About3/>
     </>
   )
 }
